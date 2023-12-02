42591678ef80884b41216794f2a703defa5100c3
Rogith <rogithsp8@gmail.com>
2023-12-02 14:22:06 +0530

    content changed from git to github

---
+++ page.js
@@ -0,0 +1,5 @@
+function change()
+{
+    document.getElementById('head').innerHTML = "Github"
+    document.getElementById('para').innerHTML = "Github is a remote repository where developer work in collaborative mode"
+}
